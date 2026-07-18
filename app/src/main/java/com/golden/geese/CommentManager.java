ef4023c68153caf62821a8e848c18a7e9503426f
Ricardo Lee <ricardo.cmlee@gmail.com>
2026-07-17 20:13:53 -0400

    Add base models and managers for core application functionality
    - Implemented `Artifact`, `Comment`, and `User` classes to define core models.
    - Added skeletons for `LikeManager`, `CommentManager`, and `SaveManager` classes.
    - Introduced support for user likes, comments, and saved artifacts.
    
    master branch will be removed to avoid confusion

---
+++ app/src/main/java/com/golden/geese/CommentManager.java
@@ -0,0 +1,12 @@
+public class CommentManager {
+    private static volatile CommentManager instance;
+    private CommentManager(){}
+
+    public static CommentManager getInstance(){
+        if(instance == null){
+            //TODO: implement singleton structure
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/golden/geese/User.java
@@ -0,0 +1,75 @@
+/*
+ * Artifact
+ * Version 1.0
+ * Bob Zhao July 17, 2026
+ *
+ * This code is provided as part of the coursework for CSCB07H3
+ * at the University of Toronto.
+ *
+ * Unauthorized reproduction, distribution, or sharing of this code is strictly
+ * prohibited and constitutes a violation of the University of
+ * Toronto Code of Behaviour on Academic Matters.
+ *
+ */
+import java.util.*;
+
+public class User {
+    private List<Artifact> likedArtifacts;
+    private String username;
+    private String pfp;
+    private List<Comment> comments;
+    private List<Artifact> savedArtifacts;
+
+
+    public User(){
+        this.username = "Anonymous User";
+        this.pfp = "https://pbs.twimg.com/media/FeToVEqX0Acjv0i.png";
+        this.likedArtifacts = new ArrayList<Artifact>(0);
+        this.comments = new ArrayList<Comment>(0);
+        this.savedArtifacts = new ArrayList<Artifact>(0);
+    }
+
+    public User(String username, String pfp){
+        this();
+        this.username = username;
+        this.pfp = pfp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPfp() {
+        return pfp;
+    }
+
+    public void setPfp(String pfp) {
+        this.pfp = pfp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(username, user.username) && Objects.equals(pfp, user.pfp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, pfp);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "likedArtifacts=" + likedArtifacts +
+                ", username='" + username + '\'' +
+                ", pfp='" + pfp + '\'' +
+                ", comments=" + comments +
+                ", savedArtifacts=" + savedArtifacts +
+                '}';
+    }
+}
