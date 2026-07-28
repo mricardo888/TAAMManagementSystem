712de864478b9c13b180ea4c78ef637267b75e4a
dongjam <jimmydong111@gmail.com>
2026-07-28 14:38:16 -0400

    refactored SignUpPresenter

---
+++ app/src/main/java/com/golden/geese/presenter/SignUpPresenter.java
@@ -0,0 +1,88 @@
+package com.golden.geese.presenter;
+
+import com.golden.geese.model.AuthRepository;
+import com.golden.geese.view.AuthView;
+
+public class SignUpPresenter {
+
+    private static SignUpPresenter instance;
+    private AuthView view;
+    private AuthRepository repo;
+
+    private String username, email, password;
+    private SignUpPresenter () {
+        this.username = "";
+        this.email = "";
+        this.password = "";
+    }
+
+    public static SignUpPresenter getSignUpPresenter () {
+        if (instance == null ) {
+            instance = new SignUpPresenter();
+            return instance;
+        }
+        return instance;
+    }
+
+    public void setView (AuthView view) {
+        this.view = view;
+    }
+
+    public void setRepo (AuthRepository repo) {
+        this.repo = repo;
+    }
+
+    public void validateName (String username) {
+        if (username == null || username.trim().isEmpty()) {
+            view.showError("Username cannot be empty.");
+            return;
+        }
+        this.username = username;
+        view.nextStep();
+    }
+
+    public void validateEmail (String email) {
+        if (email == null || email.trim().isEmpty()) {
+            view.showError("Email cannot be empty.");
+            return;
+        }
+        this.email = email;
+        view.nextStep();
+    }
+
+    public void validatePassword (String password) {
+        if (password == null || password.length() < 6) {
+            view.showError("Password must be at least 6 characters.");
+            return;
+        }
+        this.password = password;
+        signup();
+    }
+
+    private void signup() {
+        view.showLoading();
+
+//        repo.signUp(email, username, password, new AuthCallBack() {
+//            @Override
+//            public void onSuccess(User user) {
+//                view.goToHome();
+//                completeProcess();
+//            }
+//
+//            @Override
+//            public void onError(String message) {
+//                view.showError(message);
+//            }
+//        });
+    }
+
+    public void onDestroy () {
+        view = null;
+    }
+
+    public void completeProcess () {
+        // Need to call once user has created account and signed in
+        // So that presenter memory and all data from signup process is cleaned
+        instance = null;
+    }
+}
